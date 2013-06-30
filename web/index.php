8477d90f34762db740eb6ce55d2f4df548f13fc1
LoreLLo <lorello@openweb.it>
2013-07-01 01:38:36 +0200

    A first running version

--- web/index.php
+++ web/index.php
@@ -10,21 +10,30 @@ require_once __DIR__ . '/../vendor/ipfilter.class.php';
 # to use Request object
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Silex\Provider\MonologServiceProvider;
 use Aws\Common\Enum\Region;
 use Aws\Silex\AwsServiceProvider;
+use Symfony\Component\Yaml\Dumper;
 
 # Create the app instance
 $app = new Silex\Application();
 
+$env = getenv('APP_ENV') ?: 'prod';
 
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/$env.yaml"));
+/*
+$app->register(new MonologServiceProvider(), array(
+        'monolog.logfile' => __DIR__.'/../log/silex.log',
+));
+ */
 $app->register(new AwsServiceProvider(), array(
-    'aws.config' => '../aws-config.json'
+    'aws.config' => __DIR__.'/../aws-config.json'
 ));
 // Note: You can also specify a path to a config file (e.g., 'aws.config' => '/path/to/aws/config/file.php')
 
 
 # Turn on debugging
-$app['debug'] = true;
+# $app['debug'] = true;
 
 $allowed_ips[]='31.198.139.0/28';       # telecom netcruise @ Prato
 $allowed_ips[]='46.226.179.80/28';      # viking @ Prato
@@ -47,18 +56,34 @@ $allowed_ips[]='172.16.0.0/12';         # private network
 $allowed_ips[]='10.0.0.0/8';            # private network
 
 
+$app->before(
+    function () use ($app) {
+
+        $app['ip.allowed'] = array();
+        $app['ip.aws'] = array();
+
+        // load current networks
+        $app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/data.yaml"));
+    }
+);
+
+$app->after(
+    function () use ($app) {
+    }
+);
+
 $app->get(
     '/',
     function () {
-        $help = "Sintax:\nGET /is-valid-ip/{IP}\n\n";
+        $help = "Sintax:\nGET /is-allowed/{IP}\n\n";
         return $help;
     }
 );
 
 $app->get(
-    '/is-my-ip/{ip}',
-    function ($ip) use ($allowed_ips) {
-        $filter = new IPFilter($allowed_ips);
+    '/is-allowed/{ip}',
+    function ($ip) use ($app) {
+        $filter = new IPFilter(array_merge($app['ip.allowed'], $app['ip.aws']));
         if ($filter->check($ip)) {
             return 'TRUE';
         }
@@ -67,18 +92,50 @@ $app->get(
 )->assert('ip', '^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$');
 
 $app->get(
-    '/awsips',
+    '/update/aws',
     function () use ($app) {
+        # TODO: catch errors
         $client = $app['aws']->get('Ec2');
         $iterator = $client->getIterator('describeInstances');
         foreach($iterator as $object) {
             if (isset($object['PublicIpAddress'])) {
-                $result_array[] = $object['PublicIpAddress'];
+                $result[] = $object['PublicIpAddress'];
             }
         }
-        return implode("\n", $result_array);
+        $data = array(
+            'ip.allowed' => $app['ip.allowed'],
+            'ip.aws' => $result
+        );
+        $yaml = new Dumper();
+        file_put_contents(__DIR__.'/../config/data.yaml', $yaml->dump($data, 2));
+        # TODO: redirect to list/aws if OK?
+        return 'OK';
     }
 );
+
+$app->get(
+    '/list/{name}',
+    function ($name) use ($app) {
+        return implode("\n", $app['ip.'.$name]);
+    }
+)->assert('name', '^(allowed|aws)$');
+
+$app->put(
+    '/allowed/{ip}',
+    function ($ip) use ($app) {
+        if (ip2long($ip)) {
+            $data = array(
+                'ip.allowed' => array_unique(array_merge($app['ip.allowed'], array($ip))),
+                'ip.aws' => $app['ip.aws']
+            );
+            $yaml = new Dumper();
+            file_put_contents(__DIR__.'/../config/data.yaml', $yaml->dump($data, 2));
+            return 'OK';
+        }
+        return 'KO';
+    }
+);
+
 # If your application is hosted behind a reverse proxy and you want Silex to trust the X-Forwarded-For* headers, you will need to run your application like this:
 #Request::setTrustedProxies(array('127.0.0.1'));
 $app->run();
